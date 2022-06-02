eece8be284c515291af7c20b34f0037e7697df1c
apimpnamedodogwu <edenelenwoke@gmail.com>
2022-06-02 11:29:34 -0700

    Python

---
+++ dietel/comparing_two_sets_of_input.py
@@ -0,0 +1,9 @@
+
+first_number = int(input("Enter first number here: "))
+second_number = int(input("Enter second number here: "))
+
+if first_number > second_number:
+    print(first_number, "is the largest number.")
+
+if second_number > first_number:
+    print(second_number, "is the largest number.")
